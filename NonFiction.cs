7576251a0824ccbabfbf5b2e37a8cde8bfaf60c6
DavidLabett <david.labett@gmail.com>
2024-11-19 11:12:58 +0100

    added books

--- NonFiction.cs
+++ NonFiction.cs
@@ -7,5 +7,7 @@ public class NonFiction : Shelf
         Books.Add(new Book("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", "Vintage"));
         Books.Add(new Book("Quiet: The Power of Introverts in a World That Can't Stop Talking", "Susan Cain", "Crown Publishing Group/Random House, Inc"));
         Books.Add(new Book("The Immortal Life of Henrietta Lacks", "Rebecca Skloot", "Crown Publishing Group"));
+        Books.Add(new Book("Krigar-dinosaurier", "Ruth Owen", "Hegas"));
+
     }
 }
