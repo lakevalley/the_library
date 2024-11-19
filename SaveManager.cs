d904c2c0e0c6836831640eab2f91d44dd7511636
DavidLabett <david.labett@gmail.com>
2024-11-19 10:00:05 +0100

    SaveManager class with SaveFile()

---
+++ SaveManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Library
+{
+    public class SaveManager
+    {
+        
+        public List<string> _savedBooks = new();
+        public string _saveFile = "saveFile.txt";
+
+
+        public void SaveFile(Shelf shelf)
+        {
+            foreach (Book book in shelf.Books)
+            {
+                _savedBooks.Add($"{book.Title};{book.Author};{book.Publisher}");
+                
+                
+            }
+
+            File.WriteAllLines("saveFile.txt", _savedBooks);
+            
+            foreach (string book in _savedBooks)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    
+        
+    
+    }     
+}
